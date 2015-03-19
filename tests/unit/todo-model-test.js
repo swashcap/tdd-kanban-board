0fea96ee019eac47e29d7f432374a9c2d88aec79
Cory Reed <swashcap@gmail.com>
2015-03-19 00:06:39 -0700

    Test for changing `status_code`, clean up test.

--- tests/unit/todo-model-test.js
+++ tests/unit/todo-model-test.js
@@ -6,9 +6,6 @@ module('todo');
 test('status_code of 1 is Unassigned', function (assert) {
   var subject = Todo.create({ status_code: 1 });
   assert.equal(subject.get('status'), 'Unassigned');
-});
-
-test('status_code of 2 is Assigned', function (assert) {
-  var subject = Todo.create({ status_code: 2 });
+  subject.set('status_code', 2);
   assert.equal(subject.get('status'), 'Assigned');
 });
